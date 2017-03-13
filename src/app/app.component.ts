2f6578e5da113a63513f1ec2b30f3354ec29c901
Alexander <yumako87@gmail.com>
2017-03-13 19:21:26 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent  {
+  items: any[] = [];
+  constructor(private dataService: DataService) {}
+
+
+  ngOnInit(){
+    this.dataService.getData()
+      .subscribe(
+        data => console.log(data)
+      );
+  }
+      onGetData(){
+        this.dataService.getOwnData()
+          .subscribe(
+            results => {
+              const myArray = [];
+              for (let key in results) {
+                myArray.push(results[key]);
+              }
+              this.items = myArray;
+            }
+          );
+      }
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from "@angular/http";
+import 'rxjs/Rx';
+import { Observable } from "rxjs/Rx";
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+
+@Injectable()
+export class DataService {
+
+  constructor(private http: Http) {}
+
+  getData(){
+    return this.http.get('https://randomuser.me/api/')
+    .map((response: Response) => response.json());
+  }
+
+  getOwnData() {
+    return this.http.get('https://randomuser.me/api/')
+    .map((response: Response) => response.json());
+
+  }
+}
+// https://randomuser.me/api/zb868pou?key=9L5E-WMFI-JMGR-YXG9
+// https://test-9a9cf.firebaseio.com/
+// https://randomuser.me/api/?results=10
